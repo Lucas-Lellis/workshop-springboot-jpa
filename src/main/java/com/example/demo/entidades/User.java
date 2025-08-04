ce0e5629b6eafe1c0fd9c774de5e4f7e7b38b0dc
Lucas Léllis <125090781+Lucas-Lellis@users.noreply.github.com>
2025-08-04 16:06:32 -0300

    User

---
+++ src/main/java/com/example/demo/entidades/User.java
@@ -0,0 +1,87 @@
+package com.example.demo.entidades;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class User implements Serializable {
+	
+
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String nome;
+	private String email;
+	private String numero;
+	private String senha;
+	
+	public User() {
+	}
+
+	public User(Long id, String nome, String email, String numero, String senha) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.email = email;
+		this.numero = numero;
+		this.senha = senha;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+	
+}
